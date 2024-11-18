8465bd47fa649a32bc91e9f74b91b4f4ae08d8dc
agni-noor <agniotulonio.noor@optimizely.com>
2024-11-18 13:26:04 +0600

    form component added

--- wordcloud/src/App.tsx
+++ wordcloud/src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
-import Wordcloud from './components/wordcloud'
+import Wordcloud from './components/Wordcloud'
+import Form from './components/Form'
 function App() {
 
 
   return (
     <>
-      Wordcloud app
+      <Form/>
       <Wordcloud/>
 
     </>
