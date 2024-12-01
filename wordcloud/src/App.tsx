26d778428bc75ae52383526cf80ba6a0b7b7c378
agni-noor <agniotulonio.noor@optimizely.com>
2024-12-01 17:55:19 +0600

    supabase connected

--- wordcloud/src/App.tsx
+++ wordcloud/src/App.tsx
@@ -3,14 +3,28 @@ import { createClient } from "@supabase/supabase-js";
 import "./App.css";
 import Wordcloud from "./components/Wordcloud";
 import Form from "./components/Form";
+import { Database  } from "./schema";
+const words = [
+  { text: "React", size: 40 },
+  { text: "TypeScript", size: 35 },
+  { text: "JavaScript", size: 30 },
+  { text: "Frontend", size: 25 },
+  { text: "Backend", size: 20 },
+  { text: "Node.js", size: 18 },
+  { text: "Express.js", size: 22 },
+  { text: "Material-UI", size: 28 },
+  { text: "FastAPI", size: 26 },
+  { text: "MongoDB", size: 24 },
+];
 
 
-
-
-const supabase = createClient(
+const supabase = createClient<Database>( 
   "https://xlpdtfekdtwbaxbwnbxa.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhscGR0ZmVrZHR3YmF4YnduYnhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxNzEwMzgsImV4cCI6MjA0Nzc0NzAzOH0.49LeJJS0r_Afpey859ZpshCnZYlwXYgvCaimw4g5SMs"
 );
+
+const {data} = await supabase.from("challenges").select("*")
+console.log(data)
 
 function App() {
 
